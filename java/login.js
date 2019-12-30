cf489d28730c67af05c896378cf99f430c73c864
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2019-12-30 21:51:25 +0900

    로그인
    로그인후 chat.html 이동하여 정상적인 작동을 할수있게 만듬.

--- java/login.js
+++ java/login.js
@@ -2,7 +2,8 @@ id = prompt("아이디를 입력해주세요.아이디:Hello");
 if (id === "Hello") {
   password = prompt("비밀번호를 입력해주세요.비밀번호:12");
   if (password === "12") {
-    alert("인증 했습니다.");
+    alert("로그인 성공하셨습니다.");
+    location.href = "chat.html";
   } else {
     alert("비밀번호가 틀렸습니다.");
     location.href = "index.html";
